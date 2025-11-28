dad95aea422559bf329958ca16d103b261e7f0f3
Leo Galban <leonelgalban13@gmail.com>
2025-11-28 12:43:12 -0300

    Se hicieron todas las validaciones para cada modelo

---
+++ trabajo-practico-2/src/middlewares/validator.middleware.js
@@ -0,0 +1,13 @@
+import { validationResult } from "express-validator";
+
+export const validatorMiddleware = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ message: "Errores de validación", errors: errors.array() });
+  }
+
+  next();
+};
